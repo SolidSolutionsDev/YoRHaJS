bd16a53873418e7206880f096d0b916a5a3966eb
rui orey <rui.albuquerque.dorey@gmail.com>
2019-02-01 19:41:08 +0000

    adding state for prefabs of the game

--- src/game/redux/initialState.js
+++ src/game/redux/initialState.js
@@ -14,5 +14,72 @@ const initialState = {
             allIds: ['zero']
         },
     },
-    engine: {}
+    //TODO: migrate this to json or static js
+    engine: {
+        prefabs:{
+            inputManager:{
+                components:{
+                    gameInputDispatcher:{}
+                },
+                children:{},
+            },
+            camera: {
+                components: {
+                    perspectiveCamera:{
+                        fov: 45,
+                        near: 0.1,
+                        far: 10000,
+                        position:{x:0,y: -55,z: 35},
+                        lookAt:{x:0,y: 0,z: 0},
+                    },
+                    dynamicCameraManager:{
+                        cameraSoundPath: "./assets/sound/camera_change.mp3",
+                    },
+                },
+                children: {
+                },
+            },
+            audio: {
+                components: {
+
+                },
+                children: {
+                },
+            },
+            gameManager:{
+                components:{},
+                children:{},
+            },
+            grid:{
+                components:{},
+                children:{},
+            },
+            directionalLight:{
+                components:{},
+                children:{},
+            },
+            pointLight:{
+                components:{},
+                children:{},
+            },
+            board: {
+                components: {
+                    planeGeometry:{},
+                    boardManager:{},
+                },
+                children: {
+                },
+            },
+            shooter: {
+                components: {
+                    shooterManager:{},
+                    shooterGeometry:{},
+                    shooterControls:{},
+                },
+                children: {
+                },
+            },
+
+        },
+    }
 }
