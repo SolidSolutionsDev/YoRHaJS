bf28b8b62fc66a1cd1ed5cfe1ba5b24854ff6079
rui orey <rui.albuquerque.dorey@gmail.com>
2019-02-16 10:42:40 +0000

    remove some files

--- game_old/nier/Board/PlaneGeometryComponent.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react";
-import { makeComponent } from "../ComponentHOC";
-import { PlaneGeometry } from "./PlaneGeometry";
-
-export const PlaneGeometryComponent = makeComponent(PlaneGeometry);
