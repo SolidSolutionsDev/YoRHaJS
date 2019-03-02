765eb71f4d8393b3155a8b0c5b3536ad7531411f
Rui <rui.albuquerque.dorey@gmail.com>
2019-03-02 22:18:28 +0000

    clean some files

--- game_redux_old/reducer.js
+++ game_redux_old/reducer.js
@@ -1,6 +1,6 @@
 import game from "./reducers/game";
 import { combineReducers } from "redux";
-// import { routerReducer } from 'react-router-redux_old';
+// import { routerReducer } from 'react-router-game_redux_old';
 
 export default combineReducers({
   game
