d1d6a614fe394d72ee5a51d8d8d1030f0b6b8eaf
Rui Orey <rui.albuquerque.dorey@gmail.com>
2020-04-27 03:44:39 +0100

    refactor input service and add "select" and "backSelect" events

--- src/renderer/Services/InputService.js
+++ src/renderer/Services/InputService.js
@@ -1,115 +1,53 @@
-import { Component } from "react";
+import {Component} from "react";
 
 export class InputService extends Component {
-  constructor(props) {
-    super(props);
-    window.addEventListener("mousemove", function(event) {
-      const _mouse3D = {
-        x: (event.clientX / window.innerWidth) * 2 - 1,
-        y: -(event.clientY / window.innerHeight) * 2 + 1,
-        z: 0.5
-      };
-      document.dispatchEvent(
-        new CustomEvent("mouseM", { detail: { coordinates: _mouse3D } })
-      );
-    });
+    keysToEventMap = {
+        "ArrowLeft": "lookleft",
+        "ArrowRight": "lookright",
+        "ArrowUp": "lookup",
+        "ArrowDown": "lookdown",
+        "s": "moveDown",
+        "a": "moveLeft",
+        "d": "moveRight",
+        "w": "moveUp",
+        "r": "restartGame",
+        " ": "shoot",
+        "c": "camera_change",
+        "Enter": "select",
+        "Backspace": "backSelect",
+    };
 
-    document.addEventListener("keydown", function(event) {
-      // console.log( 'Pressed: ', event,event.key );
+    constructor(props) {
+        super(props);
+        window.addEventListener("mousemove", (event) => {
+            const _mouse3D = {
+                x: (event.clientX / window.innerWidth) * 2 - 1,
+                y: -(event.clientY / window.innerHeight) * 2 + 1,
+                z: 0.5
+            };
+            document.dispatchEvent(
+                new CustomEvent("mouseM", {detail: {coordinates: _mouse3D}})
+            );
+        });
 
-      if (event.key === "ArrowLeft") {
-        document.dispatchEvent(new Event("lookleft"));
-      }
+        document.addEventListener("keydown", (event) => {
+            event.preventDefault();
+            const eventType = this.keysToEventMap[event.key];
+            if (eventType) {
+                const _eventTypeToLaunch = event.repeat ? eventType : eventType + "_keydown";
+                document.dispatchEvent(new Event(_eventTypeToLaunch));
+            }
+        });
+        document.addEventListener("keyup", (event) => {
+            event.preventDefault();
+            const eventType = this.keysToEventMap[event.key];
+            if (eventType) {
+                document.dispatchEvent(new Event(eventType + "_keyup"));
+            }
+        });
+    }
 
-      if (event.key === "ArrowRight") {
-        document.dispatchEvent(new Event("lookright"));
-      }
-      if (event.key === "ArrowUp") {
-        document.dispatchEvent(new Event("lookup"));
-      }
-
-      if (event.key === "ArrowDown") {
-        document.dispatchEvent(new Event("lookdown"));
-      }
-      // console.log(event.key);
-      if (event.key === "s") {
-        document.dispatchEvent(new Event("moveDown"));
-      }
-
-      if (event.key === "a") {
-        document.dispatchEvent(new Event("moveLeft"));
-      }
-
-      if (event.key === "d") {
-        document.dispatchEvent(new Event("moveRight"));
-      }
-
-      if (event.key === "w") {
-        document.dispatchEvent(new Event("moveUp"));
-      }
-
-      if (event.key === "r") {
-        document.dispatchEvent(new Event("restartGame"));
-      }
-
-      if (event.key === " ") {
-        document.dispatchEvent(new Event("shoot"));
-      }
-
-      if (event.key === "c") {
-        document.dispatchEvent(new Event("camera_change"));
-      }
-    });
-    document.addEventListener("keyup", function(event) {
-      // console.log( 'Pressed: ', event.key );
-
-      if (event.key === "ArrowLeft") {
-        document.dispatchEvent(new Event("lookleft_keyup"));
-      }
-
-      if (event.key === "ArrowRight") {
-        document.dispatchEvent(new Event("lookright_keyup"));
-      }
-      if (event.key === "ArrowUp") {
-        document.dispatchEvent(new Event("lookup_keyup"));
-      }
-
-      if (event.key === "ArrowDown") {
-        document.dispatchEvent(new Event("lookdown_keyup"));
-      }
-
-      if (event.key === "s") {
-        document.dispatchEvent(new Event("moveDown_keyup"));
-      }
-
-      if (event.key === "a") {
-        document.dispatchEvent(new Event("moveLeft_keyup"));
-      }
-
-      if (event.key === "d") {
-        document.dispatchEvent(new Event("moveRight_keyup"));
-      }
-
-      if (event.key === "w") {
-        document.dispatchEvent(new Event("moveUp_keyup"));
-      }
-
-      if (event.key === "r") {
-        document.dispatchEvent(new Event("restartGame_keyup"));
-      }
-
-      if (event.key === " ") {
-        document.dispatchEvent(new Event("shoot_keyup"));
-      }
-
-      if (event.key === "c") {
-        document.dispatchEvent(new Event("camera_change_keyup"));
-      }
-    });
-  }
-
-  render() {
-    return null;
-    // return <div  tabIndex="0"  onKeyDown={(keys,prevState)=> {console.log("a",keys);}}  />; // other way
-  }
+    render() {
+        return null;
+    }
 }
