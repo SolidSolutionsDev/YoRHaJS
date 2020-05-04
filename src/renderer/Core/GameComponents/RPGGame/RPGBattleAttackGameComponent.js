86055f54f397c6030ac0f53373eea3c83b3db14a
Rui Orey <rui.albuquerque.dorey@gmail.com>
2020-05-04 18:20:40 +0100

    basic RPGBattleCharacterGameComponent and RPGBattleAttackGameComponent

---
+++ src/renderer/Core/GameComponents/RPGGame/RPGBattleAttackGameComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export class RPGBattleAttackGameComponent extends React.Component {
+        // to attach to all attacks
+        // deals with communication between players
+}
+
+RPGBattleAttackGameComponent.propTypes = {
+    type:PropTypes.string.isRequired,
+};
---
+++ src/renderer/Core/GameComponents/RPGGame/RPGBattleCharacterGameComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export class RPGBattleCharacterGameComponent extends React.Component {
+    // to manage battle character
+    // load graphics? connect with a [[ObjectLoaderMesh]] ?
+    // commands?
+}
+
+RPGBattleCharacterGameComponent.propTypes = {
+    type:PropTypes.string.isRequired,
+};
