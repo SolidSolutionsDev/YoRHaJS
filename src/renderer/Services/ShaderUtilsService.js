f53792f5abe9ad9c7bbb208a2b69d144d28e98bb
Rui Orey <rui.albuquerque.dorey@gmail.com>
2020-05-27 17:08:26 +0100

    crt shader wip

--- src/renderer/Services/ShaderUtilsService.js
+++ src/renderer/Services/ShaderUtilsService.js
@@ -4,6 +4,39 @@ import * as THREE from "three";
 
 export class ShaderUtilsService extends Component {
 
+    crtShaderPass = "varying vec2 vUv;\n" +
+        "uniform sampler2D inputTex;\n" +
+        "uniform float iTime;\n" +
+        "\n" +
+        "float gradient(vec2 p) {\n" +
+        "    return 1. - length(p / 3.);\n" +
+        "}\n" +
+        "\n" +
+        "float scanline(vec2 p) {\n" +
+        "    float s = 1. - abs(sin(iTime * 2. + p.y * 3.));\n" +
+        "    return s > 0.7 ? s : 0.;\n" +
+        "}\n" +
+        "\n" +
+        "float smallline(vec2 p) {\n" +
+        "    return abs(sin(iTime * 5. - p.y * 450.) * 0.9);\n" +
+        "}\n" +
+        "\n" +
+        "vec2 curve(vec2 p) {\n" +
+        "    p = (p - 0.5) * 2.0;\n" +
+        "    p *= 1.1;    \n" +
+        "    p.x *= 1.0 + pow((abs(p.y) / 5.0), 2.0);\n" +
+        "    p.y *= 1.0 + pow((abs(p.x) / 4.0), 2.0);\n" +
+        "    p  = (p / 2.0) + 0.5;\n" +
+        "    p =  p * 0.92 + 0.04;\n" +
+        "    return p;\n" +
+        "}\n" +
+        "\n" +
+        "void main() {\n" +
+        "    vec2 p = curve(vUv);\n" +
+        "    vec4 t = texture2D(inputTex, p);  \n" +
+        "    gl_FragColor = mix(t, (t  + scanline(p) * 0.02 - smallline(p) * 0.02) * gradient((p - 0.5) * 2.), t.a);\n" +
+        "}";
+
     basicVertexShader = "" +
         "varying vec2 vPosition;\n" +
         "void main() {\n" +
