c4a27167a315c5d67d178d7d2b5201a56105ee60
Rui Orey <rui.albuquerque.dorey@gmail.com>
2020-05-28 13:32:46 +0100

    remove old files

--- game_old/logic/components/states/BallStates.js
+++
@@ -1,8 +0,0 @@
-const states = {
-  start: { mass: 0, nextState: this.moving },
-  moving: { nextState: this.colliding },
-  colliding: { nextState: this.attached, mass: 0 },
-  attached: { mass: 0 }
-};
-
-export default states;
--- game_redux_old/reducer.js
+++
@@ -1,8 +0,0 @@
-import game from "./reducers/game";
-import { combineReducers } from "redux";
-// import { routerReducer } from 'react-router-game_redux_old';
-
-export default combineReducers({
-  game
-  // router: routerReducer
-});
